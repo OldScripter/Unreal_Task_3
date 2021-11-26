7d986bf158125101f839f0b6bc37164d1945cc4e
OldScripter <Oldscripter@gmail.com>
2021-11-27 00:02:46 +0300

    Task is done 1.0.1

--- Unreal_Task_3.cpp
+++ Unreal_Task_3.cpp
@@ -1,20 +1,26 @@
-﻿// Unreal_Task_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Name request:
+    std::string nameRequest{ "Please enter your name : \n" };
+    std::cout << nameRequest;
+    std::string characterName;
+    std::getline(std::cin, characterName);
+
+    // Greetings:
+    std::string greeting{ "Hi, " };
+    std::cout << "\n" << greeting << characterName << "!\n";
+
+    // First variant of analisys realization:
+    int nameLength = characterName.length();
+    std::cout << "Your name's length: " << nameLength << "\n\n";
+    std::cout << "First symbol in your name: " << characterName[0] << "\n";
+    std::cout << "Last symbol in your name: " << characterName[nameLength - 1] << "\n";
+
+    // Alternative variant of analisys realization:
+    std::cout << "\n(Or if we are using iterators):\n";
+    std::cout << "First symbol in your name: " << *characterName.begin() << "\n";
+    std::cout << "Last symbol in your name: " << *(characterName.end() - 1) << "\n";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
